c4488b9a40bee90edff2f38296f8f0841a6b4775
Romain Failla <romain.failla@efrei.net>
2017-12-03 01:30:35 +0100

    Adding SFML to the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <MovementRecognition.hpp>
 #include <Skeleton.hpp>
 
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,10 +12,24 @@
 int main(int argc, char **argv) {
     Skeleton *skeleton(new Skeleton("Tracker0", "localhost"));
     MovementRecognition *recognition(new MovementRecognition(skeleton));
+    sf::RenderWindow w(sf::VideoMode(400, 400), "SARFML");
+    sf::CircleShape shape(200.f);
 
-    for (;;) {
+    shape.setFillColor(sf::Color::Green);   
+
+    while (w.isOpen()) {
         skeleton->refresh();
         recognition->launchRecognition();
+
+        sf::Event evt;
+        while (w.pollEvent(evt)) {
+            if (evt.type == sf::Event::Closed)
+                w.close();
+        }
+
+        w.clear();
+        w.draw(shape);
+        w.display();
     }
 
     delete recognition;
