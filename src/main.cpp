ff26b163ba30a866e170b9e950570662d6d2c365
Romain Failla <romain.failla@efrei.net>
2017-12-03 18:38:31 +0100

    Setting the graphical indicator when an arm is raised

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <MovementRecognition.hpp>
 #include <Skeleton.hpp>
 
 #include <SFML/Graphics/CircleShape.hpp>
@@ -11,19 +10,32 @@
 
 int main(int argc, char **argv) {
     Skeleton *skeleton(new Skeleton("Tracker0", "localhost"));
-    MovementRecognition *recognition(new MovementRecognition(skeleton));
     sf::RenderWindow w(sf::VideoMode(400, 400), "SARFML");
     sf::CircleShape shape(200.f);
-
-    shape.setFillColor(sf::Color::Green);   
+    sf::Color color(sf::Color::Green);
 
     while (w.isOpen()) {
         skeleton->refresh();
-        recognition->launchRecognition();
+
+        if (skeleton->refreshed()) {
+            if (skeleton->elbowLeft().pos[1] > skeleton->shoulderLeft().pos[1] && skeleton->handLeft().pos[1] > skeleton->shoulderLeft().pos[1]) {
+                std::cout << "Left arm raised!" << std::endl;
+                color = sf::Color::Red;
+            } else if (skeleton->elbowRight().pos[1] > skeleton->shoulderRight().pos[1] && skeleton->handRight().pos[1] > skeleton->shoulderRight().pos[1]) {
+                std::cout << "Right arm raised!" << std::endl;
+                color = sf::Color::Red;
+            } else {
+                color = sf::Color::Blue;
+            }
+        }
+
+        shape.setFillColor(color);
 
         sf::Event evt;
         while (w.pollEvent(evt)) {
             if (evt.type == sf::Event::Closed)
+                w.close();
+            else if (evt.type == sf::Event::KeyPressed && evt.key.code == sf::Keyboard::Escape)
                 w.close();
         }
 
@@ -32,7 +44,6 @@ int main(int argc, char **argv) {
         w.display();
     }
 
-    delete recognition;
     delete skeleton;
 
     return 0;
